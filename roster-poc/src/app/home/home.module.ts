d5db9136892f9c002d41f53db0b4130e7a67cc0e
Lucas Mota <lm@codenato.tech>
2022-10-05 17:47:28 -0300

    first commit

---
+++ roster-poc/src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { NgxSmoothDnDModule } from 'ngx-smooth-dnd';
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    NgxSmoothDnDModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
