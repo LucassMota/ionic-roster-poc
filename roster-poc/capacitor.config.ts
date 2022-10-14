e2f04f0fd65c66e312199163532e32f501d17049
Murilo <murilokiyomura@live.com>
2022-10-14 14:36:02 -0300

    sqlite - windows location database

--- roster-poc/capacitor.config.ts
+++ roster-poc/capacitor.config.ts
@@ -4,7 +4,13 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'roster-poc',
   webDir: 'www',
-  bundledWebRuntime: false
-};
+  bundledWebRuntime: false,
+  plugins: {
+    CapacitorSQLite: {
+      electronMacLocation: "/YOUR_DATABASES_PATH",
+      electronWindowsLocation: "C:\\Temp\\CapacitorDatabases",
+      electronLinuxLocation: "/home/CapacitorDatabases"
+    }
+  }};
 
 export default config;
